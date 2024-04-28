7471a47213f615bbe1745d58ed41afc5c3665301
Rafael Minuesa <rafael.minuesa@gmail.com>
2024-04-29 00:44:06 +0200

    Update admin-page.php
    Revamp admin

--- admin-page.php
+++ admin-page.php
@@ -9,13 +9,19 @@ function cdnjs_script_loader_options_page() {
     ?>
     <div class="wrap">
         <h2>CDNJS Script Loader</h2>
-        <h3>To check available scripts and their versions, visit <a href="https://cdnjs.com/" target="_blank">cdnjs.com</a>.</h3>
         <form action="options.php" method="post">
             <?php
             settings_fields('cdnjs_script_loader_options');
             do_settings_sections('cdnjs_script_loader');
-            submit_button();
             ?>
+            <div id="script-fields">
+                <div class="script-field">
+                    <input type="text" name="cdnjs_script_loader_settings[scripts][]" placeholder="Script Name (e.g., jquery)" />
+                    <input type="text" name="cdnjs_script_loader_settings[versions][]" placeholder="Version (e.g., 3.5.1)" />
+                </div>
+            </div>
+            <button type="button" id="add-script-field">Add More</button>
+            <?php submit_button(); ?>
         </form>
     </div>
     <?php
@@ -25,31 +31,25 @@ add_action('admin_init', 'cdnjs_script_loader_admin_init');
 
 function cdnjs_script_loader_admin_init() {
     register_setting('cdnjs_script_loader_options', 'cdnjs_script_loader_settings', 'cdnjs_script_loader_sanitize');
-    add_settings_section('script_settings', 'Script Configuration', 'cdnjs_script_loader_section_text', 'cdnjs_script_loader');
-    add_settings_field('cdnjs_script_loader_script', 'Scripts to Load', 'cdnjs_script_loader_setting_string', 'cdnjs_script_loader', 'script_settings');
-    add_settings_field('cdnjs_script_loader_versions', 'Script Versions', 'cdnjs_script_loader_setting_versions', 'cdnjs_script_loader', 'script_settings');
-}
-
-function cdnjs_script_loader_section_text() {
-    echo '<p>Enter the scripts you want to load from CDNJS (comma separated, e.g., jquery, bootstrap).</p>';
-}
-
-function cdnjs_script_loader_setting_string() {
-    $options = get_option('cdnjs_script_loader_settings');
-    $scripts = is_array($options) && isset($options['scripts']) ? $options['scripts'] : '';
-    echo "<input id='scripts' name='cdnjs_script_loader_settings[scripts]' size='40' type='text' value='{$scripts}' />";
-}
-
-function cdnjs_script_loader_setting_versions() {
-    $options = get_option('cdnjs_script_loader_settings');
-    $versions = is_array($options) && isset($options['versions']) ? $options['versions'] : '';
-    echo "<input id='versions' name='cdnjs_script_loader_settings[versions]' size='40' type='text' value='{$versions}' />";
-    echo '<p>Enter the versions for each script respectively, separated by commas (e.g., 3.4.1, 4.5.2).</p>';
 }
 
 function cdnjs_script_loader_sanitize($input) {
+    // Sanitize each input field
     $new_input = array();
-    $new_input['scripts'] = sanitize_text_field($input['scripts']);
-    $new_input['versions'] = sanitize_text_field($input['versions']);
+    if (isset($input['scripts']) && is_array($input['scripts'])) {
+        foreach ($input['scripts'] as $key => $script) {
+            $new_input['scripts'][$key] = sanitize_text_field($script);
+        }
+    }
+    if (isset($input['versions']) && is_array($input['versions'])) {
+        foreach ($input['versions'] as $key => $version) {
+            $new_input['versions'][$key] = sanitize_text_field($version);
+        }
+    }
     return $new_input;
 }
+
+function cdnjs_script_loader_admin_scripts() {
+    wp_enqueue_script('cdnjs-admin-js', plugin_dir_url(__FILE__) . 'admin-scripts.js', array('jquery'), null, true);
+}
+add_action('admin_enqueue_scripts', 'cdnjs_script_loader_admin_scripts');
